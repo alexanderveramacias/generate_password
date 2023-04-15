27984601ca37e97c43c3ca46c7971a494b5f6c57
XanderVerMac <vera.macias26.av@gmail.com>
2023-04-15 10:40:22 -0500

    random key generation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import string
 import random
+
 letters=string.ascii_letters
 numbers=string.digits
 characters=string.punctuation
 
 def generate_password(length):
     all_characters=letters+numbers+characters
-    print(all_characters)
-generate_password(0)
+    password="".join(random.choice(all_characters) for index in range(length))
+    return password
+generate_password(10)
+
