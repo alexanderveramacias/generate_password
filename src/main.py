4dfb7a6411b5dab47d7e4e8d37e232976fb51a3e
XanderVerMac <vera.macias26.av@gmail.com>
2023-04-15 10:23:39 -0500

    define a function to match all characters

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 import string
+import random
 letters=string.ascii_letters
 numbers=string.digits
 characters=string.punctuation
+
+def generate_password(length):
+    all_characters=letters+numbers+characters
+    print(all_characters)
+generate_password(0)
