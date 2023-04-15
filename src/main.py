83d10b2c4afd29d66ea97d3e9634d3b53f84be1d
XanderVerMac <vera.macias26.av@gmail.com>
2023-04-15 11:59:49 -0500

    installed and used the colorama module

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import generate_password 
+import colorama
+
+print(colorama.Fore.LIGHTGREEN_EX+"welcome to password generator 😀 ")
 length_password=int(input("minimum entry 10 characters: "))
 if length_password < 10:
     print("password not accepted")
 else:
     password=generate_password.generate_password(length_password)
-    print(password)
+    print(colorama.Fore.LIGHTCYAN_EX+password)
+    print("this is the password that has been generated")
+
 
